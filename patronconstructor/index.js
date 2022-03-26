6269d0a9a1b94d30cfba55cca84c5cbc15e54436
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-03-26 07:52:30 -0600

    Fisrt commit

---
+++ patronconstructor/index.js
@@ -0,0 +1,12 @@
+class Miclase {
+    constructor() {
+        this.propiedad = 1;
+        this.metodo = () => {
+            console.log('Hola Mundo');
+        }
+    }
+}
+
+const instancia = new Miclase();
+
+console.log(instancia);
