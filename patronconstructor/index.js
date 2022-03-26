91ea77ab85429af2cf3c68ea5d786ed16409e5a7
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-03-26 08:13:30 -0600

    prototypes

--- patronconstructor/index.js
+++ patronconstructor/index.js
@@ -3,16 +3,32 @@ class Miclase {
         this.propiedad = pro1;
         this.propiedad = pro2;
     }
-
-
     metodo = () => {
         console.log('Hola Mundo');
     }
 }
 
 const instancia = new Miclase(4, 7);
-
 console.log(instancia);
-
-
 instancia.metodo();
+
+// Prototipos
+
+Object.prototype.mostrar = function() {
+    console.log(this);
+}
+
+String.prototype.cortar = function() {
+    try {
+        return this.replace(/^\s+|\s+$/g, '');
+    } catch (error) {
+        return this;
+    }
+}
+
+const x = {
+    a: 1
+};
+const y = '    jajaja   '.cortar();
+x.mostrar();
+y.mostrar();
