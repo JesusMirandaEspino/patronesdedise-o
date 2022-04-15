07ad5f3528be481e218699b3ab7d5d572f58d15c
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-04-15 10:06:20 -0500

    patron mediador

--- patroncomportamiento/index.js
+++ patroncomportamiento/index.js
@@ -1,6 +1,6 @@
 // Observador
 
-
+const { listeners } = require("process");
 
 const user = new user();
 
@@ -8,7 +8,6 @@ const init = () => {
     user.on( 'login', userLoggedIn );
     user.on( 'login', retrieveinit );
 }
-
 
 const userLoggedIn = () => {
     // code
@@ -20,13 +19,32 @@ const retrieveinit = () => {
 
 app.init();
 
-
 const login  = () => {
-
-
 
     user.trigger('login');
 }
 
-
 login();
+
+
+// Mediador
+const emiter = ( () => {
+    const topic = {};
+    const hOp = topic.hasOwnProperty; 
+    return {
+        on: (topic, _listeners) => {
+            if( !hOp.call(topic, topics ) ) topics[topic] = [];
+            topics[topic].push(_listeners);
+        },
+        emmit: ( topic, info ) => {
+            if( !hOp.call(topic, topics ) ) return
+            topics[topic].forEach(item => {
+                item(info != undefined ? info : {})
+            })
+        }
+    }
+})();
+
+
+emiter.on( 'hola', x => console.log(x) );
+emiter.emit('event', { hola: 'hola' });
